cb31bcc16926919d55fb120a8c86a139cd268a88
NieAnSHOW <42022984+NieAnSHOW@users.noreply.github.com>
2024-10-11 21:55:14 +0800

    新增部分功能与组件

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,4 +1,17 @@
 export type User = {
-  username : string;
-  password : string;
+	username : string;
+	password : string;
 };
+
+export type FileInfo = {
+	name : string,
+	size : number,
+	is_dir : boolean,
+	modified : string,
+	created : string,
+	sign : string,
+	thumb : string,
+	type : number,
+	hashinfo : string,
+	hash_info : any
+}
